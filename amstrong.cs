4e84ecf8485eee45e23ff47869ea494f2b70b204
Kalyan102003 <159612759+Kalyan102003@users.noreply.github.com>
2026-03-15 20:56:03 +0530

    amstrong Number

---
+++ amstrong.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amstrong_number
+{
+    public class amstrong
+    {
+        public int AmstrongNumber(int numb)
+        {
+            int temp = numb;
+            int sum = 0;
+
+            while (temp > 0)
+            {
+                int digit = temp % 10;
+                sum += digit * digit * digit;
+                temp /= 10;
+            }
+            return sum;
+            
+        }
+    }
+}
