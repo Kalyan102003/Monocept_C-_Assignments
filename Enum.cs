9f4f221a7ad0c7eaeb52031ea77859c39c081ad2
Kalyan102003 <159612759+Kalyan102003@users.noreply.github.com>
2026-03-16 20:20:18 +0530

    Enum

---
+++ Enum.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnumAndArrayAssignments
+{
+    enum Planet
+    {
+        MERCURY,
+        VENUS,
+        EARTH,
+        MARS,
+        JUPITER,
+        SATURN,
+        URANUS,
+        NEPTUNE
+    }
+
+    class Enums
+    {
+        public double GetGravity(Planet planet)
+        {
+            switch (planet)
+            {
+                case Planet.MERCURY: return 0.38;
+                case Planet.VENUS: return 0.91;
+                case Planet.EARTH: return 1.0;
+                case Planet.MARS: return 0.38;
+                case Planet.JUPITER: return 2.37;
+                case Planet.SATURN: return 0.93;
+                case Planet.URANUS: return 0.92;
+                case Planet.NEPTUNE: return 1.12;
+                default: return 1;
+            }
+        }
+
+        public void EnumProgram()
+        {
+            Console.Write("Enter Earth weight: ");
+            double earthWeight = Convert.ToDouble(Console.ReadLine());
+
+            foreach (Planet p in System.Enum.GetValues(typeof(Planet)))
+            {
+                if (p == Planet.EARTH) continue;
+
+                double weight = earthWeight * GetGravity(p);
+
+                Console.WriteLine($"Weight on {p}: {weight:F1} kg");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,44 +1,16 @@
-﻿using _11_march_assignments;
+﻿using EnumAndArrayAssignments;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
-        //ProductDetails P = new ProductDetails();
-        //P.takeValues();
-        //P.GetValues();
+        Enums enums = new Enums();
+        enums.EnumProgram();
 
 
+        int[] inputMarks = { 78, 85, 90, 88, 76 };
+        ArrayOfStudentGrades s1 = new ArrayOfStudentGrades("Swapnil Kulkarni", "R102", inputMarks);
+        s1.PrintReport();
 
-        //Book B = new Book();
-        //B.ProcessBooks();
-
-
-        //BankAccount game = new BankAccount("Game", 0);
-        //game.StartGame();
-
-
-
-        //Console.WriteLine("Select Course Type 1/2/3 to select :");
-        //Console.WriteLine("1. Online");
-        //Console.WriteLine("2. In-Person");
-        //Console.WriteLine("3. Lab");
-
-        //int choice = Convert.ToInt32(Console.ReadLine());
-
-        //Course course = null;
-
-        //if (choice == 1)
-        //    course = new OnlineCourse();
-        //else if (choice == 2)
-        //    course = new InPersonCourse();
-        //else
-        //    course = new LabCourse();
-
-        //course.Enroll();
-
-
-        Account a = new Account("temp", 0, "savings");
-        a.StartBank();
     }
 }
