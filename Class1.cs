fa569ee356f9e72f003f78ab0ba79269d3115924
Kalyan102003 <159612759+Kalyan102003@users.noreply.github.com>
2026-03-16 10:03:02 +0530

    constructur overloading

--- Class1.cs
+++ Class1.cs
@@ -1,70 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ArrayOFStuedntsFees
+namespace ConstructorOverLoading
 {
-    public class FeesArray
+    public class ConstructoroverLoading
     {
-        public int[] InputFees = new int[5];
-        public int[] CalculatedFees = new int[5];
 
-        public int Counter = 1;
-
-        public void Feesarray()
+        public ConstructoroverLoading()
         {
-            for (int i = 0; i < InputFees.Length; i++)
-            {
-                while (true)
-                {
-                    Console.WriteLine($"Enter Fee of Student {Counter}: ");
-                    int input = int.Parse(Console.ReadLine());
-
-                    if (input < 5000 || input > 10000)
-                    {
-                        Console.WriteLine("Error: Fees must be between 5000 and 10000. Enter again.");
-                    }
-                    else
-                    {
-                       
-                        InputFees[i] = input;
-
-                        
-                        if (input > 7000)
-                        {
-                            CalculatedFees[i] = input - (input * 5 / 100);
-                        }
-                        else
-                        {
-                            CalculatedFees[i] = input;
-                        }
-
-                        Counter++;
-                        break;
-                    }
-                }
-            }
-
-            Console.WriteLine("\nOriginal Fees:");
-            for (int i = 0; i < InputFees.Length; i++)
-            {
-                Console.WriteLine(InputFees[i]);
-            }
-
-            Console.WriteLine("\nFees After Discount:");
-            for (int i = 0; i < CalculatedFees.Length; i++)
-            {
-                Console.WriteLine(CalculatedFees[i]);
-            }
+            Console.WriteLine("This is the default constructor.");
         }
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
+        public ConstructoroverLoading(string message)
         {
-            FeesArray obj = new FeesArray();
-            obj.Feesarray();
-
-            Console.ReadLine();
+            Console.WriteLine("This is a parameterized constructor. Message: " + message);
+        }
+        public ConstructoroverLoading(int number)
+        {
+            Console.WriteLine("This is another parameterized constructor. Number: " + number);
         }
     }
 }
