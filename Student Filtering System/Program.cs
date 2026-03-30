63f0e66526030b17addbf95919251dd12ad24179
Kalyan102003 <159612759+Kalyan102003@users.noreply.github.com>
2026-03-30 17:18:52 +0530

    Predicate Demo student

---
+++ Student Filtering System/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static March27_LinkedList.DelegatePractice;
+using static March27_LinkedList.Deligatee;
+
+namespace March27_LinkedList
+{
+    internal class Program
+    {
+
+
+        static bool isEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        static void Main(string[] args)
+        {
+            //DelegatePractice DP = new DelegatePractice();
+            //MyDelegate MyD = DP.Hello;
+            //CalcDel CD = DP.AddDel;
+            //CD(1, 2);
+            //Console.WriteLine(" ");
+            //CD += DP.SubDel;
+            //CD(2, 3);
+
+            //Console.WriteLine(" ");
+            //CD += DP.MulDel;
+            //CD(4, 5);
+
+            //Console.WriteLine(" ");
+            //CD += DP.DivDel;
+            //CD(6, 7);
+
+
+            //Action<string> A = DP.Display;
+            //A("Hello");
+
+            //DP.show("kalyan");
+            //--------------------------------------------------------------------------
+            List<ActionDelegate> list = new List<ActionDelegate>();
+             list.Add(new ActionDelegate("Kalyan",01,55,23));
+            list.Add(new ActionDelegate("Ram", 02, 65, 23));
+            list.Add(new ActionDelegate("Shyam", 05, 45, 15));
+            list.Add(new ActionDelegate("Aryan", 06, 90, 20));
+
+            foreach (var i in list)
+            {
+                Console.WriteLine("Name :" +i.Name);
+                Console.WriteLine("id : "+i.Id);
+                Console.WriteLine("marks : " +i.marks);
+                Console.WriteLine("age : " +i.Age);
+            }
+
+
+
+            Predicate<ActionDelegate> check = x => x.marks > 60;
+
+            List<ActionDelegate> result = list.FindAll(check);
+            foreach (var i in result)
+            {
+                Console.WriteLine(" Marks Scored Greater than 60 : "+i.Name);
+
+            }
+
+            Predicate<ActionDelegate> AgeCheck = x => x.Age < 18;
+            List<ActionDelegate> Ageresult = list.FindAll(AgeCheck);
+
+            foreach (var i in Ageresult)
+            {
+                Console.WriteLine("Student Age Below 18 : "+i.Name);
+            }
+            bool Nameresult = list.Exists(
+                x => x.Name.StartsWith("A", StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine("Is name starting with A exists : " + Nameresult);
+
+
+            Predicate<ActionDelegate> NameCheck = x => x.Name.StartsWith("A");
+            List<ActionDelegate> NameResult = list.FindAll(NameCheck);
+            foreach(var i in NameResult)
+            {
+                Console.WriteLine("The Name Starts With A is : "+ i.Name);
+            }
+
+
+            //---------------------------------------------------------------------
+
+            //Deligatee D = new Deligatee();
+            //DelCalc d;
+
+
+            //Console.WriteLine("\n normal product ");
+            //d = D.Multiply;
+            //d(20, 30);
+
+            //Console.WriteLine("\n normal Reminder ");
+            //d = D.Divisioin;
+            //d(20, 30);
+
+            //Console.WriteLine("\n MultiCasting ");
+            //d += D.sum;
+            //d(10, 20);
+
+            //Console.WriteLine("\n remove MultiCasting ");
+            //d -= D.sum;
+            //d(7, 8);
+
+            //Console.WriteLine("\n Division Multicasting");
+            //d += D.Multiply;
+
+            //d(2, 3);
+
+
+
+            //StudentLinkiedList sl = new StudentLinkiedList();
+            //sl.UserInput();
+
+
+
+        }
+
+
+    }
+}
---
+++ Student Filtering System/studentLinkiedList.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace March27_LinkedList
+{
+    internal class StudentLinkiedList
+    {
+        public string StudentName;
+        public int StudentId;
+        public int StudentAge;
+        public int StudentMarks;
+        public static LinkedList<StudentLinkiedList> std = new LinkedList<StudentLinkiedList>();
+
+
+        public  void UserInput()
+        {
+            while (true)
+            {
+                Console.WriteLine("\nSelect 1 : Add students");
+                Console.WriteLine("Select 2 : Display Student Details");
+                Console.WriteLine("Select 3 : Update Student Details");
+                Console.WriteLine("Select 4 : Delete Student Details");
+                Console.WriteLine("Select 5 : Exit");
+
+                int.TryParse(Console.ReadLine(), out int UserInput);
+
+                if (UserInput == 1)
+                {
+                    AddStudents();
+                }
+                else if (UserInput == 2)
+                {
+                    DisplayStudents();
+                }
+                else if (UserInput == 3)
+                {
+                    UpdateStudent();
+                }
+                else if (UserInput == 4)
+                {
+                    RemoveStudent();
+                }
+                else if (UserInput == 5)
+                {
+                    Console.WriteLine("Exited Successfully");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+            }
+        }
+
+        public  void AddStudents()
+        {
+            Console.WriteLine("Enter Number of students : ");
+            int.TryParse(Console.ReadLine(), out int NumberOfStudents);
+
+            for (int i = 0; i < NumberOfStudents; i++)
+            {
+                Console.WriteLine("Enter Name of Student : ");
+                string Name = Console.ReadLine();
+
+                Console.WriteLine("Enter Id of student : ");
+                int.TryParse(Console.ReadLine(), out int Id);
+
+                Console.WriteLine("Enter student Age : ");
+                int.TryParse(Console.ReadLine(), out int Age);
+
+                Console.WriteLine("Enter student Marks : ");
+                int.TryParse(Console.ReadLine(), out int Marks);
+                StudentLinkiedList student = new StudentLinkiedList()
+                {
+                    StudentName = Name,
+                    StudentId = Id,
+                    StudentAge = Age,
+                    StudentMarks = Marks
+                };
+
+                std.AddFirst(student);
+
+            }
+        }
+
+        public  void DisplayStudents()
+        {
+            foreach (var item in std)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("Student Name : " + item.StudentName);
+                Console.WriteLine("Student Id : " + item.StudentId);
+                Console.WriteLine("Student Age : " + item.StudentAge);
+                Console.WriteLine("Student Age : " + item.StudentMarks);
+            }
+        }
+
+        public  void UpdateStudent()
+        {
+            Console.WriteLine("Enter Student Name to Update : ");
+            string searchName = Console.ReadLine();
+
+            foreach (var item in std)
+            {
+                if (item.StudentName == searchName)
+                {
+                    Console.WriteLine("Enter New Name : ");
+                    item.StudentName = Console.ReadLine();
+
+                    Console.WriteLine("Enter New Id : ");
+                    item.StudentId = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter New Age : ");
+                    item.StudentAge = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Updated Successfully");
+                }
+            }
+        }
+
+        public  void RemoveStudent()
+        {
+            Console.WriteLine("Enter student Name to Remove : ");
+            string RemoveStudent = Console.ReadLine();
+
+            var node = std.First;
+
+            while (node != null)
+            {
+                var next = node.Next;
+
+                if (node.Value.StudentName == RemoveStudent)
+                {
+                    std.Remove(node);
+                    Console.WriteLine("Removed Successfully");
+                }
+
+                node = next;
+            }
+
+
+            // another method to remove values
+
+            //foreach(var i in std)
+            //{
+            //    if(RemoveStudent == i.StudentName)
+            //    {
+            //        std.Remove(i);
+            //        Console.WriteLine("Removed sucessfully");
+            //    }
+            //}
+
+            Console.WriteLine("Updated List");
+
+            foreach (var item in std)
+            {
+                Console.WriteLine("Student Name : " + item.StudentName);
+                Console.WriteLine("Student Id : " + item.StudentId);
+                Console.WriteLine("Student Age : " + item.StudentAge);
+                Console.WriteLine("Student Age : " + item.StudentMarks);
+            }
+        }
+    }
+
+
+}
