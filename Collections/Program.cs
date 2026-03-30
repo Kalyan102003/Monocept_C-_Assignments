4083e966ebc5cde883ac52613008f9ba223aa822
Kalyan102003 <159612759+Kalyan102003@users.noreply.github.com>
2026-03-30 16:41:53 +0530

    Add files via upload

---
+++ Collections/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace Evaluation_01_march25_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //amstrongNumber amstr = new amstrongNumber();
+            //amstr.CheckAmstrong();
+
+            //Factorial fact = new Factorial();
+            //fact.factCheck();
+
+            //publicPrivateProtected pp = new publicPrivateProtected();
+
+            //Enumm E = new Enumm();
+            //E.EnumTest();
+
+            //MethodOverLoadingOverriding ol = new MethodOverLoadingOverriding();
+            //ol.overload("bow Bow");
+            //ol.overload("bow bow", 5);
+            //ol = new Overriding();
+            //ol.overload("meow meow");
+
+
+            //Params keyword
+            //ParamsRefOut p = new ParamsRefOut();
+            //p.Names("k", "l", "M");
+            //p.Ages(1, 2, 3, 4);
+
+
+            //ref parameter
+            //int A = 20;
+            //p.refp(ref A);
+            //Console.WriteLine(A);
+
+
+            //Out parameter
+            //int C;
+            //int B;
+            //p.testOut(out C, out B);
+            //Console.WriteLine(C);
+            //Console.WriteLine(B);
+
+
+            ////jagged Array
+            //JaggedArray jaa = new JaggedArray();
+            //jaa.JArray();
+
+
+            ////is Keyword 
+            //object H = 2;
+            //if (H is int) Console.WriteLine(true);
+
+            //interface3 inf = new interface3();
+            //inf.Employee();
+            //inf.Hell(30);
+
+            //exceptionHandeling eh = new exceptionHandeling();
+
+            //NonGeneric_ArrayList Al = new NonGeneric_ArrayList();
+            //Al.StudentDetails();
+
+            //nongeneric_Hashtable HT = new nongeneric_Hashtable();
+            //HT.studenthashtable();
+
+            //Stackk s = new Stackk();
+            //s.BooksStack();
+
+            //Quauee q = new Quauee();
+            //q.TicketQuaue();
+
+            //sortedlist s = new sortedlist();
+            //s.empsortedlist();
+
+            //GenericList gl = new GenericList();
+            //gl.NumList();
+
+            //GenericDictionary gd = new GenericDictionary();
+            //gd.UserIn();
+
+            Linkedlist li = new Linkedlist();
+            li.UserIn();
+        }
+    }
+}
