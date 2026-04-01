8f2e18d17155cc8250722107c4d4f6d997674d5f
Kalyan102003 <159612759+Kalyan102003@users.noreply.github.com>
2026-04-01 22:06:28 +0530

    Add files via upload

---
+++ MultiCastDelegate/MessageDelegate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MulticastDegligate_and_Func
+{
+    internal class MessageDelegate
+    {
+        public string Message;
+
+        public string SendEmail(string Message)
+        {
+            this.Message = Message;
+            Console.WriteLine("Email Sent : " + Message);
+            return Message;
+        }
+
+        public string SendSMS(string Message)
+        {
+            this.Message = Message;
+            Console.WriteLine("SMS Sent : " + Message);
+            return Message;
+        }
+
+        public string Whatsapp(string Message)
+        {
+            this.Message = Message;
+            Console.WriteLine("Whatsapp Sent : " + Message);
+            return Message;
+        }
+    }
+}
---
+++ MultiCastDelegate/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MulticastDegligate_and_Func
+{
+    internal class Program
+    {
+        public delegate string Notify(string Message);
+        static void Main(string[] args)
+        {
+
+            //Func<double, double> calculateDiscount;
+            //Func<double, double, double> calculateFinalPrice;
+            //FuncDiscount f = new FuncDiscount();
+            //calculateDiscount = f.Discount;
+            //calculateFinalPrice = f.FinalPrice;
+            //double dis = calculateDiscount(2000);
+            //double result = calculateFinalPrice(f.discount,f.Price);
+            //Console.WriteLine("your Discount "+ dis);
+            //Console.WriteLine("your Final Price "+result);
+
+            //------------------------------------------------------------
+            MessageDelegate MD = new MessageDelegate();
+            Notify notify;
+            notify = MD.SendEmail;  
+            notify += MD.SendSMS;    
+            notify += MD.Whatsapp;   
+            notify("Hello Kalyan");
+        }
+    }
+}
