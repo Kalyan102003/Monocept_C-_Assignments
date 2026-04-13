badaca0ec45a65dd0e9e471e675581e751aafaf7
Kalyan102003 <159612759+Kalyan102003@users.noreply.github.com>
2026-04-13 20:57:53 +0530

    Add files via upload

---
+++ MovieBooking/Movie.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MovieAssignment
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int YearOfRelease { get; set; }
+        public string Genre { get; set; }
+
+      
+        public static Movie ReadFromConsole()
+        {
+            var movie = new Movie();
+
+            Console.Write("Enter Id: ");
+            int id;
+            while (!int.TryParse(Console.ReadLine(), out id))
+            {
+                Console.Write("Invalid Id. Enter numeric Id: ");
+            }
+            movie.Id = id;
+
+            Console.Write("Enter Name: ");
+            movie.Name = Console.ReadLine();
+
+            Console.Write("Enter Year of release: ");
+            int year;
+            while (!int.TryParse(Console.ReadLine(), out year))
+            {
+                Console.Write("Invalid year. Enter numeric year: ");
+            }
+            movie.YearOfRelease = year;
+
+            Console.Write("Enter Genre: ");
+            movie.Genre = Console.ReadLine();
+
+            return movie;
+        }
+    }
+}
---
+++ MovieBooking/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> MoviesList = new List<Movie>();
+
+            Console.WriteLine("Welcome to movie store developed by : Kalyan");
+            Console.WriteLine("-----------------------------------------------");
+
+            while (true)
+            {
+                Console.WriteLine("1.Add Movie \r\n" +
+                    "2.Display All Movies\r\n" +
+                    "3.Find Movie by ID\r\n" +
+                    "4.Remove Movie by ID\r\n" +
+                    "5.Clear All Movies\r\n" +
+                    "6.Exit\r\n");
+                Console.WriteLine("-----------------------------------------------");
+
+                int.TryParse(Console.ReadLine(), out int Input);
+
+                switch (Input)
+                {
+                    case 1:
+                        MoviesList.Add(Movie.ReadFromConsole());
+                        Console.WriteLine("Movie added.");
+                        Console.WriteLine("-----------------------------------------------");
+                        break;
+                    case 2:
+                        if (MoviesList.Count == 0)
+                        {
+                            Console.WriteLine("No movies to display.");
+                        }
+                        else
+                        {
+                            foreach (var m in MoviesList)
+                            {
+                                Console.WriteLine($"Id: {m.Id}\nName: {m.Name}\nYear: {m.YearOfRelease}\nGenre: {m.Genre}");
+                                Console.WriteLine("-----------------------------------------------");
+                            }
+                        }
+                        break;
+                    case 3:
+                        Console.Write("Enter Id: ");
+                        int.TryParse(Console.ReadLine(), out int IdInput);
+                        var found = MoviesList.Find(m => m.Id == IdInput);
+                        if (found is null)
+                        {
+                            Console.WriteLine("Movie not found.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Id: {found.Id}\nName: {found.Name}\nYear: {found.YearOfRelease}\nGenre: {found.Genre}");
+                        }
+                        Console.WriteLine("-----------------------------------------------");
+                        break;
+
+                    case 4:
+                        Console.Write("Enter Id: ");
+                        int.TryParse(Console.ReadLine(), out int InputId);
+                        var removed = MoviesList.RemoveAll(m => m.Id == InputId);
+                        Console.WriteLine(removed > 0 ? "Movie(s) removed." : "No movie with that Id.");
+                        Console.WriteLine("-----------------------------------------------");
+                        break;
+
+                    case 5:
+                        MoviesList.Clear();
+                        Console.WriteLine("All movies cleared.");
+                        Console.WriteLine("-----------------------------------------------");
+                        break;
+                    case 6:
+                        return;
+                    default:
+                        Console.WriteLine("Invalid selection. Please choose a number from 1 to 6.");
+                        Console.WriteLine("-----------------------------------------------");
+                        break;
+                }
+            }
+        }
+    }
+}
